961f0b46319587a6b2db53e352a015a4c278a2a5
goodgirlwannabe <16521529@mahasiswa.itb.ac.id>
2022-11-09 01:06:07 +0700

    updated inputgambar, matrix, main

--- src/InputGambar.py
+++ src/InputGambar.py
@@ -1,17 +1,19 @@
 import cv2
 import os
-import glob
+from Matrix import *
+
+def list_files(directory):
+    list_of_files = []
+    for folder, direct, file in os.walk(directory):
+        for filename in file:
+            list_of_files.append(os.path.join(folder, filename))
+    return list_of_files
+
 
 def load_images(folder):
     images = []
-    for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder,filename))
+    for image in list_files(folder):
+        img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
         if img is not None:
             images.append(img)
     return images
-
-files = ".\dataset"
-
-for filename in glob.iglob(f'{files}/*'):
-    image = load_images(filename)
-print(image)
